77a1d7b7f563e69967c714a1ef6fb9f0dca4fa7d
4loops <colpeterson@gmail.com>
2017-06-25 13:56:09 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This code simulates different strategies for the coffee problem proposed by FiveThirtyEight.
+
+[!Graph Results](fig.png)
+
+If you want to maximize the amount of coffee you drink, it appears you should take as much as possible.
---
+++ coffee.py
@@ -0,0 +1,35 @@
+import random
+
+NUM_ITERATIONS = 10000000
+NUM_PLAYERS = 1000
+
+strategy = [random.random() for i in range(NUM_PLAYERS)]
+coffee = [0.0]*NUM_PLAYERS
+fills = [0] * NUM_PLAYERS
+
+pot = 1.0
+for i in range(NUM_ITERATIONS):
+    player = random.randint(0,NUM_PLAYERS-1)
+    pot -=  strategy[player]
+    if pot < 0:
+        pot = 1.0
+        fills[player] += 1
+        continue
+    coffee[player] += strategy[player]
+
+scores = zip(strategy, coffee)
+scores = sorted(scores, key=lambda x: x[0])
+
+x = []
+y = []
+for a, b in scores:
+    x.append(a)
+    y.append(b)
+
+import matplotlib.pyplot as plt
+plt.plot(x,y)
+plt.title("Five Thirty Eight Coffee Simulation over "+str(NUM_ITERATIONS)+" Iterations")
+plt.xlabel("Strategy")
+plt.ylabel("Amount of coffee consumed")
+plt.savefig("fig.png")
+#plt.show()
